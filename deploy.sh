8afcb9284bd4968ffdfcf1b2246fa40fd63812ad
Mikeyudex <miguel.yudex@gmail.com>
2025-04-15 14:24:44 -0500

    se agrega deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+#crear archivo de logs
+LOG_FILE="deploy.log"
+
+echo "🗓️ $(date '+%Y-%m-%d %H:%M:%S') - Inicio del despliegue" > "$LOG_FILE"
+
+echo "instalando dependencias" >> "$LOG_FILE"
+
 # instarlar dependencias npm install --force --silent
 npm install --force --silent
 
+echo "compilando app" >> "$LOG_FILE"
+
 # compilar la app o servicio
 npm run build
+
+echo "despliegue de app" >> "$LOG_FILE"
 
 # ejecutar comando de despliegue
 npm run deploy
